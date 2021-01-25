046aa2ec20258d8e9a4299ae0b66b382a10091e0
= <=>
2021-01-25 13:41:11 +0100

    Adding time feature

--- date.py
+++ date.py
@@ -2,3 +2,7 @@ from datetime import date
 
 today = date.today()
 print("Today's date:", today)
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
